b11c241113872beba92f0cbce15e8607a2507961
xizher <xizher@163.com>
2021-01-21 22:43:38 +0800

    feat: 优化矩阵类属性和方法

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,8 +1,13 @@
 export class Matrix {
 
   _array = []
+  _targetExtent = null
 
-  constructor (arr, rowCount, colCount) {
+  constructor (arr, rowCount, colCount, targetExtent) {
+    this.rowCount = rowCount
+    this.colCount = colCount
+    this._targetExtent = targetExtent
+
     let index = 0
     for (let i = 0; i < rowCount; i++) {
       this._array[i] = []
@@ -15,6 +20,45 @@ export class Matrix {
   get(row) {
     return col => {
       return this._array[row][col]
+    }
+  }
+
+  geoXYToSceneXY (point) {
+    const [x, y] = [
+      Math.round((point.x - this._targetExtent.xmin) * (this.colCount - 0) / (this._targetExtent.xmax - this._targetExtent.xmin) + 0),
+      Math.round(((this._targetExtent.ymax - (point.y - this._targetExtent.ymin) - this._targetExtent.ymin) * (this.rowCount - 0)) / (this._targetExtent.ymax - this._targetExtent.ymin) + 0)
+    ]
+    return [x, y]
+  }
+
+  getByGeoPoint (point) {
+    const [x, y] = this.geoXYToSceneXY(point)
+
+    return this.get(y)(x)
+  }
+
+  getByGeoLine (startPoint, endPoint) {
+    const [startX, startY] = this.geoXYToSceneXY(startPoint)
+    const [endX, endY] = this.geoXYToSceneXY(endPoint)
+    const _self = this
+
+    return DDA()
+
+    function DDA () {
+      const dx = Math.abs(endX - startX)
+      const dy = Math.abs(endY - startY)
+      const k = dx > dy ? dx : dy
+      const xincre = (endX - startX) / k
+      const yincre = (endY - startY) / k
+      let x = startX
+      let y = startY
+      const arr = []
+      for (let i = 0; i < k; i ++) {
+        arr.push(_self.get(Math.round(y))(Math.round(x)))
+        x += xincre
+        y += yincre
+      }
+      return arr
     }
   }
 
